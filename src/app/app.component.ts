768a6197212be65a971ffe60ad2390a5302a5064
damianc <damianczapiewski@gmail.com>
2019-03-19 12:15:55 +0100

    class name fix @src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: `<div class="bg-success p-1 text-xs-center">
+  template: `<div class="bg-success p-1 text-center">
   				To jest aplikacja SportsStore.
   			</div>`
 })
