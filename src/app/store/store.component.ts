30941fa2b4540c5fe2c9d6cd01519d2800815764
damianc <damianczapiewski@gmail.com>
2019-03-31 03:07:33 +0200

    Add links to /cart

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+
 import { Product } from "../model/product.model";
 import { ProductRepository } from "../model/product.repository";
 import { Cart } from "../model/cart.model";
@@ -13,7 +15,7 @@ export class StoreComponent {
 	public productsPerPage = 4;
 	public selectedPage = 1;
 
-	constructor(private repository: ProductRepository, private cart: Cart) {}
+	constructor(private repository: ProductRepository, private cart: Cart, private router: Router) {}
 
 	get products(): Product[] {
 		let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
@@ -47,5 +49,6 @@ export class StoreComponent {
 
 	addProductToCart(product: Product) {
 		this.cart.addLine(product);
+		this.router.navigateByUrl('/cart');
 	}
 }
