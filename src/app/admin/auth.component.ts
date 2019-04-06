4070bb3f285f4bbc68b32392815404e363afa232
damianc <damianczapiewski@gmail.com>
2019-04-06 20:45:19 +0200

    Add Login subviews

--- src/app/admin/auth.component.ts
+++ src/app/admin/auth.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
-import { Router } from "@angular/route";
+import { Router } from "@angular/router";
 
 @Component({
 	moduleId: module.id,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,10 @@ import { StoreFirstGuard } from './storeFirst.guard';
         path: 'checkout', component: CheckoutComponent,
         canActivate: [StoreFirstGuard]
       },
+      {
+        path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule',
+        canActivate: [StoreFirstGuard]
+      },
       { path: '**', redirectTo: '/store' }
     ])
   ],
